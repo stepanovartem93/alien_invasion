f4d615e52ab9c5dba04392d6b682e8e5f35234db
stepanovartem93 <artemis6318@yandex.ru>
2019-03-26 20:45:54 +0300

    Добавил класс настроек для игры

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    #Класс для хранения всех настроек "Alien Invasion"
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 640
+        self.bg_color = (230, 230, 230)
