7890935b4c3f419d13bf0379eb8eb82eaba02b70
stepanovartem93 <artemis6318@yandex.ru>
2019-04-05 16:31:48 +0300

    Вывел в отдельную функцию обработку событий

---
+++ game_functions.py
@@ -0,0 +1,8 @@
+import sys
+import pygame
+
+def check_events():
+    """Обрабатывает нажатия клавиш и события мыши."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
